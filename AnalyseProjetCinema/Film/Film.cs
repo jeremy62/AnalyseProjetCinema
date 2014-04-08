fdfc75984909b51adb5397abb87cc19bf543b2ed
ben62920 <ben.audeval@gmail.com>
2014-04-08 09:10:13 +0200

    Mise en ligne de l'application

---
+++ AnalyseProjetCinema/Film/Film.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Film
+{
+    public class Film
+    {
+        private string affiche;
+        private string nom;
+
+        public Film(string nom, string affiche) {
+            this.nom = nom;
+            this.affiche = affiche;
+        }
+        public string getAffiche() {
+            return this.affiche;
+        
+        }
+        public string getNom()
+        {
+            return this.nom;
+
+        }
+        public void setAffiche(string Affiche)
+        {
+            this.affiche = affiche;
+
+        }
+
+        public void setNom(string Affiche)
+        {
+            this.nom = nom;
+
+        }
+
+    }
+}
