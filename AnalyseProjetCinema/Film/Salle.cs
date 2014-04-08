daff797a5b3cafc9e9bcf65b8d27859e2b08c7e6
ben62920 <ben.audeval@gmail.com>
2014-04-08 09:42:17 +0200

    maj salle

---
+++ AnalyseProjetCinema/Film/Salle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Salle
+{
+    public class Salle
+    {
+        private int nbPlace;
+        private int nbPlaceTarifNormal;
+        private int nbPlaceTarifReduit;
+        private int pourcentageReduction;
+        private double prix;
+
+        public Salle(string titre, int nbPlace, double prixPlace, string photoAffiche)
+        {
+            this.nbPlace = nbPlace;
+
+        }
+    }
+}
